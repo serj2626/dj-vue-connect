046097813d77772a562731c24606b36abc2f44d9
serj2626 <serj2626@mail.ru>
2024-11-08 15:02:52 +0300

    feat: add global import by input

--- frontend/src/components/global/index.ts
+++ frontend/src/components/global/index.ts
@@ -1,23 +1,24 @@
-import UIButton from "./UI/UIButton.vue";
-import Trends from "../Trends.vue";
-import PeopleYouMayKnow from "../PeopleYouMayKnow.vue";
-import UIAcceptButton from "./UI/UIAcceptButton.vue";
-import UIRejectButton from "./UI/UIRejectButton.vue";
+import type { App } from 'vue'
+import UIButton from './UI/UIButton.vue'
+import UIInput from './UI/UIInput.vue'
+import Trends from '../Trends.vue'
+import PeopleYouMayKnow from '../PeopleYouMayKnow.vue'
+import UIAcceptButton from './UI/UIAcceptButton.vue'
+import UIRejectButton from './UI/UIRejectButton.vue'
 
 const components = [
-    { name: "UIButton", component: UIButton },
-    { name: "Trends", component: Trends },
-    { name: "PeopleYouMayKnow", component: PeopleYouMayKnow },
-    { name: "UIAcceptButton", component: UIAcceptButton },
-    { name: "UIRejectButton", component: UIRejectButton },
-];
-
-
+  { name: 'UIButton', component: UIButton },
+  { name: 'Trends', component: Trends },
+  { name: 'PeopleYouMayKnow', component: PeopleYouMayKnow },
+  { name: 'UIAcceptButton', component: UIAcceptButton },
+  { name: 'UIRejectButton', component: UIRejectButton },
+  { name: 'UIInput', component: UIInput },
+]
 
 export default {
-    install(app) {
-        components.forEach(({ name, component }) => {
-            app.component(name, component);
-        });
-    },
-};
+  install(app: App) {
+    components.forEach(({ name, component }) => {
+      app.component(name, component)
+    })
+  },
+}
