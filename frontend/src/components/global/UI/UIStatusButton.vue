5288e122d407278b3c18ec237f5a768daa941d0f
serj2626 <serj2626@mail.ru>
2024-11-09 01:17:00 +0300

    refactor: added types for size and status

--- frontend/src/components/global/UI/UIStatusButton.vue
+++ frontend/src/components/global/UI/UIStatusButton.vue
@@ -1,12 +1,31 @@
 <script setup lang="ts">
+type Size = 'sm' | 'md' | 'lg'
+
 type StatusFriend =
   | 'Это Вы'
   | 'Ваш друг'
   | 'Вы подписаны'
   | 'Ваш подписчик'
   | 'Отправить заявку'
+
+interface IProps {
+  size: Size
+  status: StatusFriend
+}
+
+const emit = defineEmits(['delete', 'sendRequest'])
+
+defineProps<IProps>()
 </script>
 <template>
-  <div></div>
+  <button
+    :class="{
+      'text-sm py-2 px-3': size === 'sm',
+      'text-md py-4 px-6': size === 'md',
+      'text-lg py-6 px-8': size === 'lg',
+    }"
+    class="inline-block bg-purple-600 text-white rounded-lg hover:bg-purple-700"
+  >
+    <slot></slot>
+  </button>
 </template>
-<style scoped></style>
