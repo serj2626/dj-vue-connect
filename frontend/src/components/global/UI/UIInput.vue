9b45a9496c5ebaa738bb85c0f16ae7361039ee1b
serj2626 <serj2626@mail.ru>
2024-11-08 15:02:32 +0300

    feat: added props

--- frontend/src/components/global/UI/UIInput.vue
+++ frontend/src/components/global/UI/UIInput.vue
@@ -1,10 +1,20 @@
 <script lang="ts" setup>
+
+const query = defineModel()
+
+interface IProps {
+  placeholder: string
+  type?: string
+  size?: string
+}
+
+defineProps<IProps>()
 </script>
 <template>
-<div>
- 
-</div>
+  <input
+    v-model="query"
+    type="search"
+    class="p-4 w-full bg-gray-100 rounded-lg"
+    :placeholder="placeholder"
+  />
 </template>
-<style scoped>
- 
-</style>
