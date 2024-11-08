294d292225471481f0cc3906fdcd960db68cd8be
serj2626 <serj2626@mail.ru>
2024-11-08 13:53:04 +0300

    feat: added props for size by button

--- frontend/src/components/global/UI/UIButton.vue
+++ frontend/src/components/global/UI/UIButton.vue
@@ -1,14 +1,23 @@
 <script lang="ts" setup>
-defineProps({
-    text: {
-        type: [String, Number],
-        required: true
-    }
-})
+type Size = 'sm' | 'md' | 'lg'
+
+interface IProps {
+  text: string
+  size: Size
+}
+
+defineProps<IProps>()
 </script>
 <template>
-    <button class="inline-block py-4 px-6 bg-purple-600 text-white rounded-lg hover:bg-purple-700">
-      {{ text }}
-    </button>
+  <button
+    :class="{
+      'text-sm py-2 px-3': size === 'sm',
+      'text-md py-4 px-6': size === 'md',
+      'text-lg py-6 px-8': size === 'lg',
+    }"
+    class="inline-block bg-purple-600 text-white rounded-lg hover:bg-purple-700"
+  >
+    <slot></slot>
+  </button>
 </template>
 <style scoped></style>
