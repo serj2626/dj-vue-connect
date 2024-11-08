0b8defb8059e118d8ab94506aad55d4ef71794eb
serj2626 <serj2626@mail.ru>
2024-11-08 13:59:46 +0300

    feat: added global btn

--- frontend/src/components/Trends.vue
+++ frontend/src/components/Trends.vue
@@ -1,43 +1,44 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <template>
-    <div class="p-4 bg-white border border-gray-200 rounded-lg">
-        <h3 class="mb-6 text-xl">Популярное</h3>
+  <div class="p-4 bg-white border border-gray-200 rounded-lg">
+    <h3 class="mb-6 text-xl">Популярное</h3>
 
-        <div class="space-y-4">
-            <div class="flex items-center justify-between">
-                <p class="text-xs">
-                    <strong>#codewithstein</strong><br>
-                    <span class="text-gray-500">180 posts</span>
-                </p>
+    <div class="space-y-4">
+      <div class="flex items-center justify-between">
+        <p class="text-xs">
+          <strong>#codewithstein</strong><br />
+          <span class="text-gray-500">180 posts</span>
+        </p>
 
-                <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Смотреть</a>
-            </div>
+        <UIButton :size="`sm`">Смотреть</UIButton>
+      </div>
 
-            <div class="flex items-center justify-between">
-                <p class="text-xs">
-                    <strong>#codewithstein</strong><br>
-                    <span class="text-gray-500">180 posts</span>
-                </p>
+      <div class="flex items-center justify-between">
+        <p class="text-xs">
+          <strong>#codewithstein</strong><br />
+          <span class="text-gray-500">180 posts</span>
+        </p>
 
-                <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Смотреть</a>
-            </div>
+        <UIButton :size="`sm`">Смотреть</UIButton>
+      </div>
 
-            <div class="flex items-center justify-between">
-                <p class="text-xs">
-                    <strong>#codewithstein</strong><br>
-                    <span class="text-gray-500">180 posts</span>
-                </p>
+      <div class="flex items-center justify-between">
+        <p class="text-xs">
+          <strong>#codewithstein</strong><br />
+          <span class="text-gray-500">180 posts</span>
+        </p>
 
-                <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Смотреть</a>
-            </div>
+        <UIButton :size="`sm`">Смотреть</UIButton>
+      </div>
 
-            <div class="flex items-center justify-between">
-                <p class="text-xs">
-                    <strong>#codewithstein</strong><br>
-                    <span class="text-gray-500">180 posts</span>
-                </p>
+      <div class="flex items-center justify-between">
+        <p class="text-xs">
+          <strong>#codewithstein</strong><br />
+          <span class="text-gray-500">180 posts</span>
+        </p>
 
-                <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Смотреть</a>
-            </div>
-        </div>
+        <UIButton :size="`sm`">Смотреть</UIButton>
+      </div>
     </div>
+  </div>
 </template>
