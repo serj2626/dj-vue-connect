43dd1f46612640591d31a7c3d49d59540b2f5ef3
serj2626 <serj2626@mail.ru>
2024-11-09 01:10:37 +0300

    feat: created global btn for status friends

---
+++ frontend/src/components/global/UI/UIStatusButton.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+type StatusFriend =
+  | 'Это Вы'
+  | 'Ваш друг'
+  | 'Вы подписаны'
+  | 'Ваш подписчик'
+  | 'Отправить заявку'
+</script>
+<template>
+  <div></div>
+</template>
+<style scoped></style>
