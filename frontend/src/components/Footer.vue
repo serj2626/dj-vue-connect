75aea7251e5ce753d77035d31b0d9d7d0fb259ff
serj2626 <serj2626@mail.ru>
2024-11-09 17:49:57 +0300

    feat: created footer links

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,25 +1,29 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
-
+import { footerLinks } from '@/types/footer.data'
 </script>
 <template>
-    <div class="flex items-center justify-around">
-            <p class="">© 2024 Санкт Петербург.</p>
+  <div class="flex items-center justify-around">
+    <p class="">© 2024 Санкт Петербург.</p>
 
-        <div class="flex items-center  gap-4 py-8">
-            <i class="fa-brands fa-vk fa-xl" title="vk" style="color: #028af2;"></i>
-            <i class="fa-brands text-red-600 fa-instagram fa-xl" title="instagram"></i>
-            <i class="fa-brands fa-telegram fa-xl" title="telegram" style="color: #005eff;"></i>
-            <i class="fa-brands fa-github fa-xl" title="github" style="color: #000000;"></i>
-        </div>
+    <div class="flex items-center gap-4 py-8">
+      <i
+        v-for="link in footerLinks"
+        class="cursor-pointer"
+        :key="link.title"
+        :style="{ color: link.color }"
+        :class="link.class"
+        :title="link.title"
+      ></i>
     </div>
-
+  </div>
 </template>
 <style scoped>
 .fa-brands {
-    transition: scale .3s ease;
+  transition: scale 0.3s ease;
 
-    &:hover {
-        scale: 1.2;
-    }
+  &:hover {
+    scale: 1.2;
+  }
 }
 </style>
---
+++ frontend/src/types/footer.data.ts
@@ -0,0 +1,28 @@
+interface IFooterDataLInk {
+  title: string
+  color: string
+  class: string
+}
+
+export const footerLinks: IFooterDataLInk[] = [
+  {
+    title: 'vk',
+    color: 'blue',
+    class: 'fa-brands fa-vk fa-xl',
+  },
+  {
+    title: 'instagram',
+    color: '#b4370e',
+    class: 'fa-brands text-red-600 fa-instagram fa-xl',
+  },
+  {
+    title: 'telegram',
+    color: '#005eff',
+    class: 'fa-brands fa-telegram fa-xl',
+  },
+  {
+    title: 'github',
+    color: '#000000',
+    class: 'fa-brands fa-github fa-xl',
+  },
+]
